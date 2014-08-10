ed8fd72b83ff984b0a45b7d49b307075615f4f6a
seyko21 <seyko21@hotmail.com>
2014-08-10 10:15:34 -0500

    x

--- app/Labels.php
+++ app/Labels.php
@@ -103,5 +103,6 @@ define('LABEL_RV9','Dirección');
 define('LABEL_RV10','E-mail');
 define('LABEL_RV11','Ingresar dirección');
 define('LABEL_RV12','ingresar e-mail');
-define('LABEL_RV13','Sexo123456');
+define('LABEL_RV13','Sexo');
+//roger
 ?>
