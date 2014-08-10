d9e59e7b50cf8ce784f376047d8a5a8844788696
seyko21 <seyko21@hotmail.com>
2014-08-09 23:17:14 -0500

    modulo personal con opcion registro vendedor

---
+++ modules/personal/controllers/registrarVendedorController.php
@@ -0,0 +1,21 @@
+<?php
+/*
+* --------------------------------------
+* fecha: 10-08-2014 06:08:26 
+* Descripcion : registrarVendedorController.php
+* --------------------------------------
+*/    
+
+class registrarVendedorController extends Controller{
+
+    public function __construct() {
+        $this->loadModel('registrarVendedor');
+    }
+    
+    public function index(){ 
+        Obj::run()->View->render('index');
+    }
+    
+}
+
+?>
---
+++ modules/personal/views/js/registrarVendedor.js
@@ -0,0 +1,68 @@
+var registrarVendedor_ = function(){
+    
+    var _private = {};
+    
+    _private.id = 0;
+    
+    _private.config = {
+        modulo: 'personal/registrarVendedor/'
+    };
+
+    this.publico = {};
+    
+    this.publico.main = function(element){
+        simpleScript.addTab({
+            id : diccionario.tabs.[NUMERO_DE_LABEL],
+            label: $(element).attr('title'),
+            fnCallback: function(){
+                registrarVendedor.getCont();
+            }
+        });
+    };
+    
+    this.publico.getCont = function(){
+        simpleAjax.send({
+            dataType: 'html',
+            root: _private.config.modulo,
+            fnCallback: function(data){
+                $('#'+diccionario.tabs.[NUMERO_DE_LABEL]+'_CONTAINER').html(data);
+                registrarVendedor.[METODO_PARA_GRID]();
+            }
+        });
+    };
+    
+this.publico.[METODO_PARA_GRID] = function (){
+        $('#'+diccionario.tabs.[NUMERO_DE_LABEL]+'grid[NOMBRE_TABLA]').dataTable({
+            bProcessing: true,
+            bServerSide: true,
+            bDestroy: true,
+            sPaginationType: "bootstrap_full", //two_button
+            sServerMethod: "POST",
+            bPaginate: true,
+            iDisplayLength: 10,            
+            aoColumns: [
+                {sTitle: "<input type='checkbox' id='"+diccionario.tabs.[NUMERO_DE_LABEL]+"chk_all' onclick='simpleScript.checkAll(this,\"#"+diccionario.tabs.[NUMERO_DE_LABEL]+"grid[NOMBRE_TABLA]\");'>", sWidth: "1%", sClass: "center", bSortable: false},
+                {sTitle: "Campo 1", sWidth: "55%"},
+                {sTitle: "Campo 2", sWidth: "8%",  sClass: "center", bSortable: false},
+                {sTitle: "Estado", sWidth: "8%",  sClass: "center", bSortable: false},
+                {sTitle: "Acciones", sWidth: "15%", sClass: "center", bSortable: false}
+            ],
+            aaSorting: [[1, 'asc']],
+            sScrollY: "300px",
+            sAjaxSource: _private.config.modulo+'[ACCION_DE_CONTROLLER_PHP]',
+            fnDrawCallback: function() {
+                $('#'+diccionario.tabs.[NUMERO_DE_LABEL]+'grid[NOMBRE_TABLA]_filter').find('input').attr('placeholder','Buscar');
+                /*para hacer evento invisible*/
+                simpleScript.removeAttr.click({
+                    container: '#widget_'+diccionario.tabs.[NUMERO_DE_LABEL], //widget del datagrid
+                    typeElement: 'button, #'+diccionario.tabs.[NUMERO_DE_LABEL]+'chk_all'
+                });
+            }
+        });
+        setup_widgets_desktop();
+    };
+    
+    return this.publico;
+    
+};
+var registrarVendedor = new registrarVendedor_();
