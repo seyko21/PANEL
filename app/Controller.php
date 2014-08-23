447029e269dc4e77fc17458969ed48f8fdfac426
seyko21 <seyko21@hotmail.com>
2014-08-22 19:39:26 -0500

    se agrego metodo para require a controladores externos

--- app/Controller.php
+++ app/Controller.php
@@ -6,29 +6,53 @@
  * Controller.php
  * --------------------------------------
  */
-abstract class Controller{
+abstract class Controller extends Obj{
     
+    /*para crear el onjeto $call;*/
+    public function __construct() {
+        parent::__construct();
+    }
+
     #obliga a que todas las clases que heredan Controller tengan un metodo index,
     #esto es para la validacion del metodo en Request
     abstract public function index(); 
     
-    protected function loadModel($modelo){
-        $modelo = $modelo . 'Model';
-        $rutaModelo = ROOT . 'modules' . DS . Obj::run()->Request->getModulo() . DS .'models' . DS . $modelo . '.php';
+    /*para llamar controladores de un modulo a otro*/
+    protected function loadController($control){
+        $modulo = $control['modulo'];
+        $controller = $control['controller'] . 'Controller';
+        $rutaControlador = ROOT . 'modules' . DS . $modulo. DS . 'controllers' . DS . $controller . '.php';
+        
+        if (is_readable($rutaControlador)) {
+            require_once ($rutaControlador);
+            Registry::anonimous($controller); /*registro el controlador por unica vez*/
+        } else {
+            echo 'Error de Controlador: <b>'.$rutaControlador.'</b> no encontrado.';
+        }
+    }
+
+    protected function loadModel($modelo){   
+        /*se verifica si es array*/
+        if(is_array($modelo)){ /*esto es para cuando modelo se llamara desde un modulo externo*/
+            $mod = $modelo['modelo'] . 'Model';
+            
+            $rutaModelo = ROOT . 'modules' . DS . $modelo['modulo'] . DS .'models' . DS . $mod . '.php';
+        }else{/*se carga modelo desde el mismo modulo*/
+            $mod = $modelo . 'Model';
+            $rutaModelo = ROOT . 'modules' . DS . Obj::run()->Request->getModulo() . DS .'models' . DS . $mod . '.php';
+        }
         
         if(is_readable($rutaModelo)){
             require_once ($rutaModelo);
-            //$modelo = new $modelo;
-            Registry::anonimous($modelo); /*registro del $modelo por unica vez*/
+            Registry::anonimous($mod); /*registro del $modelo por unica vez*/
             
-            //return Obj::run()->$modelo;
         }else{
             throw new Exception('Error de Modelo: <b>'.$rutaModelo.'</b> no encontrado');
         }
     }
     
     protected function getLibrary($libreria){
-        $rutaLibreria = ROOT . 'libs' . DS . $libreria . DS . $libreria.'.php';
+        $rutaLibreria = ROOT . 'libs' . DS . $libreria . '.php';
         
         if(is_readable($rutaLibreria)){
             require_once ($rutaLibreria);
