55ea02a6946f7cdf2a3b012fbedd5ab8508bc860
DanielMP <DanielMP@192.168.2.202>
2014-10-03 08:32:23 -0500

    Retorno de Inversion Arreglado

--- app/LabelsD.php
+++ app/LabelsD.php
@@ -208,8 +208,8 @@ define('MIINV_1','Mis Inversiones');
 define('MIINV_2','Detalle de Inversion');
 define('LISOC_1','Liquidación de Socio');
 define('PAINV_1','Mis Paneles Invertidos');
-define('CREINV_1','Retorno de Inversión');
-define('CREINV_2','Listado de Orden de Servicio');
+define('CREIN_1','Retorno de Inversión');
+define('CREIN_2','Listado de Orden de Servicio');
 
 
 ?>
